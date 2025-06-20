16d34cc0db2979c9f94762836609b3ded40efef1
Chunchunmaru25 <clarenceduerme40@gmail.com>
2025-06-20 10:16:35 +0800

    commit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ca472190485d8c748dd0bbdd7ab52f4c3b2a9624',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -395,9 +395,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ca472190485d8c748dd0bbdd7ab52f4c3b2a9624',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
