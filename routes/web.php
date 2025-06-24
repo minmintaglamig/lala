47aaf39ef995adbb86a3b04602256e42aca88262
CurlyAsh <azhesmangalino@gmail.com>
2025-06-24 08:59:35 +0800

    dasdsa

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,22 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VehicleController;
 
-Route::get('/', fn () => view('index'));
+Route::resource('vehicles', VehicleController::class);
+
+Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect('/dashboard');
+    }
+    return view('index');
+});
 
 Route::get('/dashboard', function () {
-    $user = Auth::user();
-
-    if ($user->role === 'Admin') return redirect('/admin/dashboard');
-    if ($user->role === 'Driver') return redirect('/driver/dashboard');
-    return redirect('/client/dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('/admin/dashboard', 'admin.dashboard')->name('admin.dashboard');
-    Route::view('/driver/dashboard', 'driver.dashboard')->name('driver.dashboard');
-    Route::view('/client/dashboard', 'client.dashboard')->name('client.dashboard');
-
-    Route::prefix('admin')->name('admin.')->group(function () {
-        Route::view('/driver', 'admin.driver.index')->name('driver.index');
-        Route::view('/vehicle', 'admin.vehicle.index')->name('vehicle.index');
-        Route::view('/job', 'admin.job.index')->name('job.index');
-        Route::view('/location', 'admin.location.index')->name('location.index');
-    });
-});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -34,3 +25,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
