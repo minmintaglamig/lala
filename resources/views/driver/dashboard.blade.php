cd8d4c73d42bc89cd04c1ba01a90e15993edf8d3
iamKaylV <bscskaylvenus@gmail.com>
2025-06-27 14:51:17 +0800

    Delete dashboard.blade.php

--- resources/views/driver/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.driver')
-
-@section('title', 'Dashboard')
-
-@section('content')
-<div class="text-3xl font-bold text-[#EA2F14]">
-    Welcome, {{ Auth::user()->name }} ({{ Auth::user()->role }})
-</div>
-@endsection
